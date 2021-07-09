30a5049da30befba85507c212776d52006fe646f
esteban perez <epzmox@gmail.com>
2021-07-09 14:25:43 -0500

    prueba para produccion

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,16 @@
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 
 module.exports = {
     entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js'
+      },
+      resolve: {
+        extensions: ['.js'],
+      },
     module: {
         rules: [{
                 test: /\.css$/i,
@@ -24,6 +32,7 @@ module.exports = {
 
     plugins: [
         new HtmlWebpackPlugin({
+            inject: true,
             template: './public/index.html'
         }),
     ],
