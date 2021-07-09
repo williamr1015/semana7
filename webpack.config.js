8467f1e40b73d93417735063dfd23e6cd64bd37a
Jeison Cabarcas <ancjeison@gmail.com>
2021-07-08 23:45:20 -0500

    estilos

--- webpack.config.js
+++ webpack.config.js
@@ -2,27 +2,33 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 
 module.exports = {
-    entry: "./src/index.js",
-    module: {
-        rules: [
-          {
-            test: /\.css$/i,
-            use: ["style-loader", "css-loader"],
-          },
-          {
-            test: /\.(png|jpg|gif)$/i,
-            use: [
-              {
-                loader: "url-loader",
-                options: {
-                  limit: 8192,
-                },
-              },
-            ],
-          }
-        ],
-
+  entry: "./src/index.js",
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 8192,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        }
+      }
+    ],
+  },
     
     plugins: [
         new HtmlWebpackPlugin({
